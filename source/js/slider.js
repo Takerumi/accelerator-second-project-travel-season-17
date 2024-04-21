3130816fe363233a5d9a3090a1e83044d3dd61b3
Anastasia <mrgabrielripper@gmail.com>
2024-04-21 15:02:01 +0600

    Стилизует секцию hero под мобильное разрешение, подключает слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -6,14 +6,27 @@ const heroSwiper = new Swiper('.hero__slider', {
   direction: 'horizontal',
   loop: true,
   slidesPerView: 1,
+  watchSlidesProgress: true,
   spaceBetween: 0,
   keyboard: {
     enabled: true,
     onlyInViewport: true
   },
-  modules: [Navigation],
+  modules: [Navigation, Pagination],
   navigation: {
   },
+  pagination: {
+    el: '.hero__pagination',
+    bulletElement: 'button',
+    clickable: true,
+    bulletActiveClass: 'hero__dot--active',
+    renderBullet: function (index, className) {
+      const buttonClass = `hero__dot ${className}`;
+      return `<button class="${buttonClass}" type="button">
+        <span class="visually-hidden">Перейти к ${index + 1} слайду.</span>
+      </button>`;
+    }
+  }
 });
 
 export { heroSwiper };
